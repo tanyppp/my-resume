fd4b1b43208605ce8368d3dc4aadb734cbd5c9ad
Tanyipeng <947860982@qq.com>
2019-12-27 23:29:56 +0800

    完成第二页

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const config = require('../server-config')
+const fs = require('fs')
+console.log(config)
+
+const app = express()
+app.use(express.static(config.staticPath))
+app.get('*', (_, res) => res.end(fs.readFileSync(config.staticPath + '/index.html')))
+app.listen(config.port, () => {
+  console.log('app listen at port:' + config.port)
+})
